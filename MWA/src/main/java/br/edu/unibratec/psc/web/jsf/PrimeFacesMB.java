df2ed8e928a53967bbf1a4725a761b1e06b20ee3
pedropk <pedropk@192.168.0.37>
2018-11-16 17:25:09 -0300

    First config to use PrimeFaces. Still need some ajustments

---
+++ MWA/src/main/java/br/edu/unibratec/psc/web/jsf/PrimeFacesMB.java
@@ -0,0 +1,76 @@
+package br.edu.unibratec.psc.web.jsf;
+
+import java.io.Serializable;
+
+import javax.faces.component.html.HtmlDataTable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.unibratec.psc.model.dao.PessoaDAO;
+import br.edu.unibratec.psc.model.entity.Pessoa;
+
+@Named
+@ViewScoped
+public class PrimeFacesMB implements Serializable {
+	
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 1772316099848213676L;
+	
+	@Inject
+	private PessoaDAO daoPessoa;
+	
+	/**
+	 *  Atributos de Tela
+	 */
+	Pessoa pessoa = new Pessoa();
+	//List<Pessoa> listaPessoas;
+	private PessoasSelectedDataModel	pessoas;
+	
+	public PessoasSelectedDataModel getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(PessoasSelectedDataModel pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	public PrimeFacesMB() {
+		pessoa = new Pessoa();
+	}
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	
+	public void setPessoa(Pessoa pPessoa) {
+		this.pessoa = pPessoa;
+	}
+	
+	public PessoasSelectedDataModel getListaPessoas() {
+		return pessoas;
+	}
+	
+	public void setListaPessoas(PessoasSelectedDataModel pListaPessoas) {
+		this.pessoas = pListaPessoas;
+	}
+	
+	public void inserir() {
+		// Create a registry at Database
+		//this.daoPessoa.insert(this.pessoa);
+		this.daoPessoa.update(pessoa);
+		
+		// Retrieve all registries from Database
+		this.pessoas = new PessoasSelectedDataModel( this.daoPessoa.consultarTodasAsPessoasByInjectedEM() );
+	}
+	
+	public void delete(Pessoa pPessoa) {
+		this.daoPessoa.deleteObject(pPessoa);
+		
+		// Retrieve all registries from Database
+		this.pessoas = new PessoasSelectedDataModel( this.daoPessoa.consultarTodasAsPessoasByInjectedEM() );
+	}
+	
+}
