057bb76a8f9faab3c8e3ee9a8f04ef7f9f70b1fb
pedropk <pedropk@gmail.com>
2019-05-16 19:30:48 -0300

    Testing hashCode() returning always same with same Attribute's Values

---
+++ MWA/src/test/java/br/edu/unibratec/psc/model/entity/PessoaTest.java
@@ -0,0 +1,28 @@
+package br.edu.unibratec.psc.model.entity;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+
+import org.junit.Test;
+
+public class PessoaTest {
+
+	@Test
+	public void testHashCodeFixo() throws ParseException {
+		// Arrange
+		Pessoa pessoa = new Pessoa();
+		pessoa.setCdMatricula(12345);
+		pessoa.setCpf("987654321-09");
+		pessoa.setNome("Jerson");
+		pessoa.setDataNascimento("01/02/2003");
+		
+		// Act
+		int hashCodeObtido = pessoa.hashCode();
+		
+		// Assert
+		int resultadoEsperado = 880695151;
+		assertEquals(resultadoEsperado, hashCodeObtido);
+	}
+
+}
